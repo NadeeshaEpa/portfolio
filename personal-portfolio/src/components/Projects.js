26e6e1128c35b31021c2e9ec8d3fd487c2f2b064
unknown <2020cs049@stu.ucsc.cmb.ac.lk>
2024-11-23 20:57:12 +0530

    small changes

--- personal-portfolio/src/components/Projects.js
+++ personal-portfolio/src/components/Projects.js
@@ -91,10 +91,10 @@ export const Projects = () => {
                       <Nav.Link eventKey="first">All</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">Web Applications</Nav.Link>
+                      <Nav.Link eventKey="second">Web</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">Mobile Applications</Nav.Link>
+                      <Nav.Link eventKey="third">Mobile</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
